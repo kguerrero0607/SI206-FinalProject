d7ac606a0d5ad6d02472172822a6811b46020ad1
Kayla <kguerrero0607@gmail.com>
2018-04-19 14:55:19 -0400

    bit off more than i could chew send help

--- songkick.py
+++ songkick.py
@@ -13,7 +13,7 @@ class Artist:
         self.uri = uri
         self.d18 = d18
         self.d17 = d17
-        self.d16 = d16 
+        self.d16 = d16
         self.d15 = d15
         self.d14 = d14
 
@@ -22,6 +22,32 @@ class Artist:
         # return('{} ({} - {}) | On tour: {} | On tour until: {} | 2018 concerts: {} | 2017 concerts: {} | 2016 concerts: {} | 2015 concerts: {} | 2014 concerts: {}'.format(self.name, self.artist_id, self.uri, self.ontour, self.ontouruntil, self.d18, self.d17, self.d16, self.d15, self.d14))
 
 ################################################################################
+
+class Concert:
+    def __init__(self, venue, id):
+        self.venue = venue
+        self.id = id
+
+    def __str__(self):
+        return('{} {}'.format(self.venue, self.id))
+
+################################################################################
+
+class Cities:
+    def __init__(self, artist_id, c1, c1num, c2, c2num, c3, c3num, c4, c4num, c5, c5num):
+        self.artist_id = artist_id
+        self.c1 = c1
+        self.c1num = c1num
+        self.c2 = c2
+        self.c2num = c2num
+        self.c3 = c3
+        self.c3num = c3num
+        self.c4 = c4
+        self.c4num = c4num
+        self.c5 = c5num
+
+    def __str__(self):
+        return '{} - {} ({}), {} ({}), {} ({}), {} ({}), {} ({}),'.format(self.artist_id, self.c1, self.c1num, self.c2, self.c2num, self.c3, self.c3num, self.c4, self.c4num, self.c5, self.c5num)
 
 # search for specific artist in api to get songkick id, cache info
 try:
@@ -99,6 +125,28 @@ def get_artist_id(artist):
 
 ################################################################################
 
+try:
+    fref = open('past_gigs.json', 'r')
+    data = fref.read()
+    GIG_CACHE_DICT = json.loads(data)
+    fref.close()
+except:
+    GIG_CACHE_DICT = {}
+
+def make_past_gig_request_using_cache(url):
+    if url in GIG_CACHE_DICT:
+        return GIG_CACHE_DICT[url]
+    else:
+        resp = requests.get(url)
+        GIG_CACHE_DICT[url] = resp.text
+        dumped_json_cache = json.dumps(GIG_CACHE_DICT)
+        fref = open('past_gigs.json',"w")
+        fref.write(dumped_json_cache)
+        fref.close()
+        return GIG_CACHE_DICT[url]
+
+################################################################################
+
 def init_db():
     conn = sqlite3.connect('music.db')
     cur = conn.cursor()
@@ -146,13 +194,12 @@ def init_db():
         CREATE TABLE 'Concerts' (
             'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
             'ArtistId' INTEGER,
-            'Venue' TEXT,
-            'City' TEXT,
-            'State' TEXT,
-            'Country' TEXT,
-            'Date' TEXT
+            'Venue' TEXT
         );
     '''
+    # 'City' TEXT,
+    # 'State' TEXT,
+    # 'Country' TEXT,
     cur.execute(statement)
 
     statement = '''
@@ -239,14 +286,6 @@ def create_artists():
 
             page_soup = BeautifulSoup(concert_data[specific_artist_url], 'html.parser')
 
-            # see if artist is on tour or not
-            # content_div = page_soup.find(class_="col-8 primary")
-            # on_tour_data = content_div.find(class_='ontour')
-            # if 'no' in on_tour_data.text:
-            #     on_tour = 'No'
-            # elif 'yes' in on_tour_data.text:
-            #     on_tour = 'Yes'
-
             # get past num concerts for past 5 years
             try:
                 tour_stats_div = page_soup.find(class_="component artist-touring-stats")
@@ -274,11 +313,100 @@ def create_artists():
             artist_list.append(Artist(artist_id, display_name, on_tour, on_tour_until, uri, d18, d17, d16, d15, d14))
     return artist_list
 
+def create_concerts():
+    f2 = open('concert_data.json', 'r')
+    d2 = f2.read()
+    concert_data = json.loads(d2)
+    f2.close()
+
+    concert_list = []
+
+
+    location = ''
+    id = 0
+    for x in concert_data:
+        all_gigs_url = x + '/gigography'
+        page_html = make_past_gig_request_using_cache(all_gigs_url)
+        page_soup = BeautifulSoup(page_html, 'html.parser')
+
+        artist_name_div = page_soup.find(class_="component brief")
+        artist_name = artist_name_div.find('a').text
+        id = get_artist_id(artist_name)
+
+        past_gigs_div = page_soup.find(class_="component events-summary", id='event-listings')
+        past_gigs = past_gigs_div.find_all(class_="event-listings ")
+        for gig in past_gigs:
+
+            location_info = gig.find_all(class_="location")
+            for l in location_info:
+                location_list = l.find_all('span')
+                if ',' in location_list[0].text.strip():
+                    location = 'No venue'
+                else:
+                    location = location_list[0].text.strip()
+                concert_list.append(Concert(location, id))
+    return concert_list
+
+def create_cities():
+    f = open('artist_cache.json', 'r')
+    d = f.read()
+    artists_data = json.loads(d)
+    f.close()
+
+    f2 = open('concert_data.json', 'r')
+    d2 = f2.read()
+    concert_data = json.loads(d2)
+    f2.close()
+
+    cities_list = []
+    artist_id = 0
+    c1 = ''
+    c1num = 0
+    c2 = ''
+    c2num = 0
+    c3 = ''
+    c3num = 0
+    c4 = ''
+    c4num = 0
+    c5 = ''
+    c5num = 0
+
+    for x in artists_data:
+        print(x)
+        artist_info = artists_data[x]['resultsPage']['results']['artist'][0]
+        artist_id = artist_info['id']
+
+        for y in concert_data:
+            print(y)
+            specific_artist_url = ''
+            if x.replace(' ', '-') in y:
+                specific_artist_url = y
+            else:
+                continue
+
+            page_soup = BeautifulSoup(concert_data[specific_artist_url], 'html.parser')
+            content_div = page_soup.find(class_="component artist-touring-stats")
+            most_played_div = content_div.find_all(class_="stat")
+            # most_played_div_closer = most_played_div.find()
+            for z in most_played_div:
+                if 'Most played' in z.find('p').text:
+                    closer_look = z.find(class_="info")
+                    cities = closer_look.find_all('li')
+                    for c in cities:
+                        info = c.text.strip().split('\n')
+                        cities_list.append({artist_id: {info[0]: int(info[1][2:(len(info[1])-1)])}})
+            print(cities_list)
+        # most_played = most_played_div.find_all('li')
+        # print(most_played_div)
+
+################################################################################
+
 def insert_stuff():
     conn = sqlite3.connect('music.db')
     cur = conn.cursor()
 
     artist_list = create_artists()
+    concert_list = create_concerts()
 
     for x in artist_list:
         params = (x.artist_id, x.name, x.ontour, x.ontouruntil, x.uri, x.d18, x.d17, x.d16, x.d15, x.d14)
@@ -288,7 +416,17 @@ def insert_stuff():
         '''
         cur.execute(statement, params)
 
+    for x in concert_list:
+        params = (None, x.id, x.venue)
+        statement = '''
+            INSERT INTO 'Concerts'
+            VALUES(?, ?, ?)
+        '''
+        cur.execute(statement, params)
+
+
     conn.commit()
+
     conn.close()
 
 def insert_from_artist():
@@ -322,47 +460,31 @@ def insert_from_concert():
     concert_data = json.loads(d2)
     f2.close()
 
-    # page_html = make_concert_request_using_cache(baseurl, artist_info)
-    # page_soup = BeautifulSoup(page_html, 'html.parser')
-
+    date = ''
+    location = ''
     for x in concert_data:
         print(x)
-        page_soup = BeautifulSoup(concert_data[x], 'html.parser')
+        all_gigs_url = x + '/gigography'
+        page_html = make_past_gig_request_using_cache(all_gigs_url)
+        page_soup = BeautifulSoup(page_html, 'html.parser')
 
-        content_div = page_soup.find(class_="col-8 primary")
-        on_tour = content_div.find(class_='ontour')
-        d18 = 0
-        d17 = 0
-        d16 = 0
-        d15 = 0
-        d14 = 0
-        try:
-            tour_stats_div = page_soup.find(class_="component artist-touring-stats")
-            tour_history = tour_stats_div.find(class_="touring-activity")
-            tour_rows = tour_history.find_all('tr')
-            for x in tour_rows:
-                if x.find('td').text.strip() == '2018':
-                    d18 = int(x.find('td')['title'].split()[0])
-                elif x.find('td').text.strip() == '2017':
-                    d17 = int(x.find('td')['title'].split()[0])
-                elif x.find('td').text.strip() == '2016':
-                    d16 = int(x.find('td')['title'].split()[0])
-                elif x.find('td').text.strip() == '2015':
-                    d15 = int(x.find('td')['title'].split()[0])
-                elif x.find('td').text.strip() == '2014':
-                    d14 = int(x.find('td')['title'].split()[0])
-            print(str(d18))
-            print(str(d17))
-            print(str(d16))
-            print(str(d15))
-            print(str(d14))
+        past_gigs_div = page_soup.find(class_="component events-summary", id='event-listings')
+        past_gigs = past_gigs_div.find_all(class_="event-listings ")
+        for gig in past_gigs:
+            dates = gig.find_all(class_="with-date")
+            for y in dates:
+                date = y.text.strip()
+            location_info = gig.find_all(class_="location")
+            for l in location_info:
+                location_list = l.find_all('span')
+                if ',' in location_list[0].text.strip():
+                    location = 'No venue'
+                else:
+                    location = location_list[0].text.strip()
+                print(location)
+        # all_gigs_url = past_gigs_div.find('a')['href']
 
-        except:
-            print(str(d18))
-            print(str(d17))
-            print(str(d16))
-            print(str(d15))
-            print(str(d14))
+        # print(past_gigs)
 
 
     # for x in countries_data:
@@ -407,10 +529,14 @@ def insert_from_concert():
     # conn.close()
 
 
-# user_input = input('gimmie a band boyo: ')
-# while user_input != 'exit':
-#     make_artist_request_using_cache(user_input)
-#     (make_concert_request_using_cache(user_input))
-#     user_input = input('gimmie a band boyo: ')
+user_input = input('gimmie a band boyo: ')
+while user_input != 'exit':
+    make_artist_request_using_cache(user_input)
+    (make_concert_request_using_cache(user_input))
+    user_input = input('gimmie a band boyo: ')
+create_artists()
+create_concerts()
 init_db()
 insert_stuff()
+# for x in create_concerts():
+#     print(x)
